f748bacbb5ecc321997e550f80ecfdb3d584fb49
r_sauna_sugiyama <r.sauna.sugiyama@gmail.com>
2020-06-02 17:15:27 +0900

    HelloWorld内に好きな寿司ランキングと焼き肉ランキングを記載

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static main(String args[]){
 		System.out.println("HelloWorld");
+		System.out.println("1.しめさば 2.かれい 3.えび");
+		System.out.println("1.サガリ 2.まるちょう 3.牛タン");
 	}
 }
